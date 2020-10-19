860eb07b47010249dd12d198090337f4bb37be80
i67655685 <uobestduo_umut@outlook.com>
2020-10-19 09:54:57 +0000

    two cars / remake

--- lab2/example6.py
+++ lab2/example6.py
@@ -1,8 +1,8 @@
-distance = 300
+distance = 340
 a = 80 
 b = 70
 total_velo = a + b
-
-min_ = distance / total_velo
+hours = distance / total_velo
+min_ = hours*60
 
 print("It takes ", min_ ,"minutes.")
