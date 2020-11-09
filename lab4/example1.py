1d93405e5feb6d102bb781f9651b9706432598c5
i67655685 <uobestduo_umut@outlook.com>
2020-11-09 09:12:03 +0000

    two digits

---
+++ lab4/example1.py
@@ -0,0 +1,6 @@
+int_number = int(input("Please type a number: "))
+list_int_number = list(str(int_number))
+#print(list_int_number)
+for list_int_number in range(0,):
+
+#I just try to bring last two digits with for function.
---
+++ lab4/example2.py
@@ -0,0 +1,10 @@
+value_of_year = int(input("Please enter a year: "))
+
+if value_of_year % 4:
+  if value_of_year % 400 == 0:
+    print(value_of_year, "is a leap year!")
+  elif value_of_year % 400 != 0:   #There is some problem like that this line of code does not work with year 1900 and ı actually don tget ıt why.
+    print(value_of_year, "is not a leap year!")
+else :
+  print(value_of_year, "is a leap year!")
+
