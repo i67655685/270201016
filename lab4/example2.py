b512a69bb08fb08e63193e30066817533e0409a8
i67655685 <uobestduo_umut@outlook.com>
2020-11-09 09:15:32 +0000

    leap year

--- lab4/example2.py
+++ lab4/example2.py
@@ -3,7 +3,7 @@ value_of_year = int(input("Please enter a year: "))
 if value_of_year % 4:
   if value_of_year % 400 == 0:
     print(value_of_year, "is a leap year!")
-  elif value_of_year % 400 != 0:   #There is some problem like that this line of code does not work with year 1900 and ı actually don tget ıt why.
+  elif value_of_year % 400 != 0:   #There is some problem like that this line of code does not work with year 1900 and ı actually don't get ıt why.
     print(value_of_year, "is not a leap year!")
 else :
   print(value_of_year, "is a leap year!")
