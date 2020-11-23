6f046a774571e4a0b166e66b30686b47b28d1779
i67655685 <uobestduo_umut@outlook.com>
2020-11-23 09:37:45 +0000

    average grade

---
+++ lab6/example2.py
@@ -0,0 +1,8 @@
+number_of_stu = input("How many students ? ")
+
+
+while True:
+  for i in number_of_stu:
+    grades = input("Enter the grades and seperate by comma : ").split(",")
+  if number_of_stu == int(number_of_stu):
+    break
