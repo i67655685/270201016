4902d84e306efb0fc849229a38a715115efce06c
i67655685 <uobestduo_umut@outlook.com>
2020-11-30 09:03:32 +0000

    book dict

---
+++ lab7/example2.py
@@ -0,0 +1,6 @@
+books = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
+
+book_dict = {}
+
+# I cant handle it by myself, I will try them after the lab sections many times 
+# Actually dont understand the way the add the list elements into the dict one by one
