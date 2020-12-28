a22267f2f088771f51c5c3193d18e3b1fe6ca1a3
i67655685 <uobestduo_umut@outlook.com>
2020-12-28 09:21:05 +0000

    pascals's triangle

---
+++ lab10/example4.py
@@ -0,0 +1,11 @@
+n = eval(input("Last line of Pascal's Triangle:"))
+def triangle(n):
+  new_row = [1]
+  result = triangle(n-1)
+  last_row = result[-1]
+  for i in range(len(last_row)-1):
+    new_row.append(last_row[i] + last_row[i+1])
+  new_row += [1]
+  result.append(new_row)
+  return result
+print(triangle(n))
