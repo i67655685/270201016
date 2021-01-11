63ab68288285a192e88f40392086745af8d7130c
i67655685 <uobestduo_umut@outlook.com>
2021-01-11 09:25:47 +0000

    employee

---
+++ lab11/example2.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.set_name = name
+        self.set_age = salary
+#I really don't understand how the classes are working  
+
+class Company:
+    def __init__(self):
+        self.employee_list = []
